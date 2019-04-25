28b7cb46c1ef7d1956a052075a26cd45b8f595bc
Avinash Murugesan <avi.quid@gmail.com>
2019-04-25 18:49:15 +0530

    Update staleProps.js

--- staleProps.js
+++ staleProps.js
@@ -17,6 +17,8 @@ function Example() {
 }
 
 // Click on show alert button, then click on "click me" button multiple times. What's the value of count in Alert??
+// Check - https://codesandbox.io/s/88m334l31l
 
 // SURPRISED?
+// Solution - https://codesandbox.io/embed/qzj2nqvzyj
 // Want to learn more - https://reactjs.org/docs/hooks-faq.html#why-am-i-seeing-stale-props-or-state-inside-my-function
